f236d5fda86004bc25a45b2410da73782a088d80
John Solo <33820078+my4andle@users.noreply.github.com>
2018-06-29 21:11:19 -0600

    Update mass_browser.py
    - replace masscan with socket for reliability and to remove reliance on package
    - clean up results
    - test functionality
    - add docstring

--- mass_browser.py
+++ mass_browser.py
@@ -2,7 +2,7 @@
 """
 Usage:
   webbrowser.py    -h | --help
-  webbrowser.py    (--proto=<proto> --subnet=<subnet> --rport=<rpot> --browser=<browser> --iface=<iface>)
+  webbrowser.py    (--proto=<proto> --subnet=<subnet> --rport=<rport> --browser=<browser> --iface=<iface>)
 
 Options:
   --subnet=<subnet>     subnet
@@ -12,71 +12,95 @@ Options:
   --iface=<iface>       outbound interface for masscan
 """
 
-# TODO: clean up docstrings
-# run through test cases
-
 import os
 import webbrowser
-import masscan
+import concurrent.futures
+from ipaddress import IPv4Network
+from socket import socket, AF_INET, SOCK_STREAM
 from docopt import docopt
 from netifaces import ifaddresses, AF_INET
 
-
-def masscan_subnet(subnet: str, iface, rport: str="80"):
+def create_ip_list(subnet):
     """
-    Run masscan against a subnet to quickly return a list of IPs with open port 445.
+    Create a list of ip addresses from a give subnet.
 
-    Args:
-        subnet: A subnet to scan
-
-    Returns:
-        A list of IPv4 addresses that report TCP port open by masscan
-
-    Note:
-        Masscan command: masscan -oX - <subnet> -p <port> --adapter-ip <source interface ip>
+    Arguments:
+        subnet:         a subnet in cidr form ex: 196.168.0.0/24 
     """
-    print("Running masscan")
-    rhosts_open = []
+    print("generating an ip list from subnet: {}".format(subnet))
+    ip_list = []
+    for ip in IPv4Network(subnet):
+        ip_list.append(ip)
+    print("ip list: {}".format(ip_list))
+    return ip_list
+
+def scan_port(ip, rport, iface):
+    """
+    Scan a single host for an open port.
+
+    Arguments:
+        ip:         an ip to scan
+        rport:      remote port
+        iface       source interface for outbound traffic  
+    """
+    print("scanning: {}".format(ip))
     iface_ip = ifaddresses(iface)[AF_INET][0]['addr']
-    try:
-        my_scanner = masscan.PortScanner()
-    except masscan.PortScannerError as ex:
-        print("MASSCAN not installed to OS path: {}".format(str(ex)))
-        os._exit(1)
+    my_soc = socket(AF_INET, SOCK_STREAM)
+    my_soc.bind((iface_ip, 0))
+    my_soc.settimeout(3)
+    result = my_soc.connect_ex((ip, int(rport)))
+    if result == 0:
+        return ip
+    else:
+        pass
 
-    print("Begin masscan against subnet: {}".format(subnet))
-    try:
-        my_scanner.scan(subnet, ports=rport, arguments="--adapter-ip {}".format(iface_ip))
-        scan_results = my_scanner.scan_result['scan']
-    except masscan.PortScannerError as ex:
-        print("The masscan requires root privleges, run with sudo")
-        os._exit(1)
-    except KeyError as ex:
-        print("Masscan results do not contain scan data: {}".format(str(ex)))
-        os._exit(1)
-
-    for host in my_scanner.all_hosts:
-        try:
-            if scan_results[host]['tcp'][rport]['state'] == 'open':
-                rhosts_open.append(host)
-        except KeyError as ex:
-            print("Port {} closed on: {}".format(rport, host))
-            pass
-
-    print("Hosts with open port {}: {}".format(rport, rhosts_open))
-    return rhosts_open
+def scan_port_concurrent(ip_list, rport, iface):
+    """
+    Scan for open port concurrently.
+    
+    Arguments:
+        ip_list:    list of ipv4 targets
+        rport:      remote port
+        iface       source interface for outbound traffic        
+    """
+    print("entering concurrent port scan")
+    results_list = []
+    with concurrent.futures.ProcessPoolExecutor(max_workers=50) as pool:
+        results = {pool.submit(scan_port, str(ip), rport, iface): ip for ip in ip_list}
+        for future in concurrent.futures.as_completed(results):
+            if future.result():
+                results_list.append(future.result())
+    print(results_list)
+    return results_list
 
 def open_your_tabs(rhosts_open, browser="firefox", proto="http", rport=80):
+    """
+    Open a web browser tab for all open ports.
+
+    Arguments:
+        rhosts_open:    list of hosts with the desired port open
+        browser:        browser to open tabs for
+        proto:          http or https
+        rport:          remote port to check
+    """
+    print("time to open browser tabs")
+    print("just because the port is open, doesn't mean there is a web server")
+    print("checking http/s requests could lead to false positives")
+    print("closing browser windws is easier than opening them")
+    print("happy web surfing")
     for ip in rhosts_open:
-        if rport:
+        print("opening tab for: {}".format(ip))
+        if (rport != "80" and rport != "443" ):
             webbrowser.get(browser).open_new_tab('{}://{}:{}'.format(proto, ip, rport))
         else:
             webbrowser.get(browser).open_new_tab('{}://{}'.format(proto, ip))
 
+
 def main():
     opts = docopt(__doc__)
-    rhosts_open = masscan_subnet(
-        subnet=opts['--subnet'],
+    ip_list = create_ip_list(subnet=opts['--subnet'])
+    rhosts_open = scan_port_concurrent(
+        ip_list=ip_list,
         rport=opts['--rport'],
         iface=opts['--iface']
     )
